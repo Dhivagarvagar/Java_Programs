c72d2fa1c1d1c470b81e2181fb3fe40e04de41e5
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-15 10:41:24 +0530

    Update Max_array_value.java

--- Max_array_value.java
+++ Max_array_value.java
@@ -5,10 +5,10 @@ public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
 	int a[]=new int[n];
-	int max=0;
 	for(int i=0;i<a.length;i++) {
 		a[i]=sc.nextInt();
 	}
+	int max=a[0];
 	for(int i=0;i<a.length;i++) {
 		if(a[i]>max) {
 			max=a[i];
