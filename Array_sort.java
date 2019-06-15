28c97725749b051b79bb7eb07cbe31f1d220c9b7
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-15 11:31:15 +0530

    Add files via upload

---
+++ Array_sort.java
@@ -0,0 +1,24 @@
+package clas;
+import java.util.*;
+public class Array_sort {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	int temp=0;
+	for(int i=0;i<arr.length;i++) {
+		arr[i]=sc.nextInt();
+	}
+	for(int i=0;i<arr.length;i++) {
+		for(int j=0;j<arr.length;j++) {
+	     if(arr[i]<arr[j]) {
+	    	 temp=arr[i];
+	    	 arr[i]=arr[j];
+	    	 arr[j]=temp;
+	     }
+    }
+	}
+	for(int i=0;i<arr.length;i++)
+	System.out.print(arr[i]+" ");
+}
+}
