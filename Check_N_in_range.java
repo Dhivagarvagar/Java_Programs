574ee63ae76c52a95800f0df61e5cad9df19244f
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 12:13:31 +0530

    Update Check_N_in_range.java

--- Check_N_in_range.java
+++ Check_N_in_range.java
@@ -4,7 +4,7 @@ public class Check_N_in_range {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	int N=sc.nextInt();
-	if(N>=1  && N<=9){
+	if(N>=1  && N<=10){
 		System.out.println("yes");
 	}
 	else{
