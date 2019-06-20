decf76033c2d8762a2c971e62a7c2035bc40b6ca
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 12:10:21 +0530

    Add files via upload

---
+++ Check_N_in_range.java
@@ -0,0 +1,14 @@
+package Try;
+import java.util.*;
+public class Check_N_in_range {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int N=sc.nextInt();
+	if(N>=1  && N<=9){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+}
+}
