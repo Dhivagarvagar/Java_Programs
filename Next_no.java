4200e1f7e6b2f8a1159719a056cfa3c8d81e055f
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 12:18:48 +0530

    Add files via upload

---
+++ Next_no.java
@@ -0,0 +1,9 @@
+package Try;
+import java.util.*;
+public class Next_no {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println(a+1);
+}
+}
