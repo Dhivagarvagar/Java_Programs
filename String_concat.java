075c9e559fbf6ea548b998cf445717d41abb0d26
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 12:02:38 +0530

    Add files via upload

---
+++ String_concat.java
@@ -0,0 +1,10 @@
+package Try;
+import java.util.*;
+public class String_concat {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	String b=sc.next();
+		System.out.print(a+b);
+}
+}
