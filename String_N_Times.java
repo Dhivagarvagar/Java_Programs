792df0a7d00c6af9d91820e5e4d71fe176bbef9b
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 11:39:35 +0530

    Add files via upload

---
+++ String_N_Times.java
@@ -0,0 +1,12 @@
+package Try;
+import java.util.*;
+public class String_N_Times {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	int b=sc.nextInt();
+	for(int i=0;i<b;i++){
+		System.out.println(a);
+		}
+}
+}
