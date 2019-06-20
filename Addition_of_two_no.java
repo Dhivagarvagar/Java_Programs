c87a1655e28e944a72f47991cc1a004afd4c3173
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-20 15:32:47 +0530

    Add files via upload

---
+++ Addition_of_two_no.java
@@ -0,0 +1,10 @@
+package clas;
+import java.util.*;
+public class Addition_of_two_no {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.println(a+b);
+}
+}
