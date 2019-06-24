3f384be8cdda370d3e811c717a64c1b7365179a4
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-24 10:08:03 +0530

    Add files via upload

---
+++ Print_no_in_Words.java
@@ -0,0 +1,41 @@
+package clas;
+import java.util.*;
+public class Print_no_in_Words {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	switch(a) {
+	case 1:
+		System.out.println("One");
+		break;
+	case 2:
+		System.out.println("Two");
+		break;
+	case 3:
+		System.out.println("Three");
+		break;
+	case 4:
+		System.out.println("Four");
+		break;
+	case 5:
+		System.out.println("Five");
+		break;
+	case 6:
+		System.out.println("Six");
+		break;
+	case 7:
+		System.out.println("Seven");
+		break;
+	case 8:
+		System.out.println("Eight");
+		break;
+	case 9:
+		System.out.println("Nine");
+		break;
+	case 10:
+		System.out.println("Ten");
+		break;
+	}
+	}
+}
+
