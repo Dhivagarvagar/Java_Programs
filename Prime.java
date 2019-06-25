c8223686761a047496e03c4f657d4891d76960d5
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-25 17:21:36 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,19 @@
+package NewTry;
+import java.util.*;
+public class Prime {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);		
+	int a=sc.nextInt();
+	boolean b=true;
+	for(int i=2;i<a;i++) {
+		if(a%i==0) {
+			b=false;
+			break;
+		}
+	}
+	if(!b)
+	System.out.println("no");
+	else
+	System.out.println("yes");
+	}
+}
