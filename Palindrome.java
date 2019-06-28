a795e773db0b48945fa6371cf0431e4309702727
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-28 13:46:47 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,11 @@
-package clas;
+package NewTry;
 import java.util.*;
 public class Palindrome {
-	public static void main(String[] args) {
-	 Scanner sc=new Scanner(System.in);
-	 String a=sc.next();
-	 String b="";
-	 int n=a.length();
-	 for(int i=n-1;i>=0;i--) {
-		 b+=a.charAt(i);
-	 }
-	 System.out.println(a.equalsIgnoreCase(b) ? "Palindrome":"not Palindrome" );
-	}
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	StringBuilder str=new StringBuilder(a);
+	String c=str.reverse().toString();
+	System.out.println(a.equals(c)? "palindrome": "not Palindrome");	
+}
 }
