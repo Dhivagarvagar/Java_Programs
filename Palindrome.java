860809ebb267a6ee4f4e26677d679977a4724619
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-28 13:50:29 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	String a=sc.next();
 	StringBuilder str=new StringBuilder(a);
 	String c=str.reverse().toString();
-	System.out.println(a.equals(c)? "palindrome": "not Palindrome");	
+	System.out.println(a.equals(c)? "yes": "no");	
 }
 }
