54a7d0e20524034c01fcf540b927210ed7503f0b
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-07-10 11:55:33 +0530

    Add files via upload

---
+++ Multiple_of_13.java
@@ -0,0 +1,16 @@
+package NewTry;
+import java.util.*;
+public class Multiple_of_13 {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int a=sc.nextInt();
+	if(a%13==0) 
+	{
+		System.out.println("yes");
+	}
+	else 
+	{
+		System.out.println("no");
+	}
+}
+}
