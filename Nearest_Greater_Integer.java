050796b1fd87e701d69d96bd3a902be168c96d3e
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-07-10 10:31:04 +0530

    Add files via upload

---
+++ Nearest_Greater_Integer.java
@@ -0,0 +1,10 @@
+package NewTry;
+import java.util.*;
+public class Nearest_Greater_Integer {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	double a=sc.nextDouble();
+	int b=(int)Math.ceil(a);
+	System.out.println(b);
+}
+}
